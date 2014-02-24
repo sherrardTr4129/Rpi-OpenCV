67c6fd26fd59fbe9b1e694f9c64b4a561bad7da8
Trevor <tws4129@rit.edu>
2014-02-24 03:08:14 -0500

    Update button_read_example.py
    fixed a blatant error

--- button_read_example.py
+++ button_read_example.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO ## Import GPIO library
 import time ## Import 'time' library. Allows us to use 'sleep'
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+GPIO.setup(17, GPIO.IN) ## Setup GPIO Pin 7 to OUT
 
 def checkButton():
     #check button state on pin 17
